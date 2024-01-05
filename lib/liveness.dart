1d77a06df1840df0416e69df7501f821504c314a
Guillian Drouin <guillian.drouin@boursorama.fr>
2024-01-05 09:32:23 +0100

    update export

--- lib/liveness.dart
+++ lib/liveness.dart
@@ -1,5 +1,6 @@
 export "controllers/liveness_controller.dart";
 export 'models/liveness/condition/eyes/liveness_eye_blink.dart';
+export 'models/liveness/condition/liveness_condition.dart';
 export 'models/liveness/condition/liveness_condition_result.dart';
 export 'models/liveness/condition/rotation/liveness_face_center.dart';
 export 'models/liveness/condition/rotation/liveness_face_turn_left.dart';
